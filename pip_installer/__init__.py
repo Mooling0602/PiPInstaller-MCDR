e29499b61d2627acd0299d2c8561a9293d6b0f00
木泠 <clemooling@outlook.com>
2025-08-03 14:45:20 +0800

    feat: support multi packages install

--- pip_installer/__init__.py
+++ pip_installer/__init__.py
@@ -3,23 +3,24 @@ import sys
 
 from mcdreforged.api.all import *
 
-
 builder = SimpleCommandBuilder()
 
+
 def on_load(server: PluginServerInterface, prev_module):
-    builder.arg('package', Text)
+    builder.arg('package', GreedyText)
     builder.register(server)
     server.logger.info("PyPI安装器准备就绪！")
+
 
 @builder.command('!!pip install <package>')
 @builder.command('!!pipi <package>')
 def on_install_pypi(src: CommandSource, ctx: CommandContext):
     if src.is_console:
-        package = ctx['package']
+        packages = ctx['package'].split()
         cmd = [
             sys.executable,
             '-m', 'pip', 'install',
-            package
+            *packages
         ]
         subprocess.Popen(cmd).wait()
         src.reply("PyPI包安装指令执行完成！")
