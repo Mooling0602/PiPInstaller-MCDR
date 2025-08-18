b51071aee399602e41ffbd7834572271755ba10a
木泠 <clemooling@outlook.com>
2025-08-18 11:41:18 +0800

    fix: improve type check

--- pip_installer/__init__.py
+++ pip_installer/__init__.py
@@ -1,27 +1,32 @@
 import subprocess
 import sys
 
-from mcdreforged.api.all import *
+from typing import Any
+from mcdreforged.api.all import (
+    SimpleCommandBuilder,
+    PluginServerInterface,
+    CommandSource,
+    CommandContext,
+    GreedyText,
+    RText,
+    RColor,
+)
 
 builder = SimpleCommandBuilder()
 
 
-def on_load(server: PluginServerInterface, prev_module):
-    builder.arg('package', GreedyText)
+def on_load(server: PluginServerInterface, prev_module: Any):
+    builder.arg("package", GreedyText)
     builder.register(server)
     server.logger.info("PyPI安装器已加载！")
 
 
-@builder.command('!!pip install <package>')
-@builder.command('!!pipi <package>')
+@builder.command("!!pip install <package>")
+@builder.command("!!pipi <package>")
 def on_install_pypi(src: CommandSource, ctx: CommandContext):
     if src.is_console:
-        packages = ctx['package'].split()
-        cmd = [
-            sys.executable,
-            '-m', 'pip', 'install',
-            *packages
-        ]
+        packages: list[str] = ctx["package"].split()
+        cmd: list[str] = [sys.executable, "-m", "pip", "install", *packages]
         subprocess.Popen(cmd).wait()
         src.reply("PyPI包安装指令执行完成！")
     else:
